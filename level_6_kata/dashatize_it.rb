fa1f8c9145a1d59564c5b33826c19f884bb9cdbd
Jordan Beck <jordanfbeck@gmail.com>
2021-08-26 12:16:54 -0600

    Adds completed dashatize_it kata

--- level_6_kata/dashatize_it.rb
+++ level_6_kata/dashatize_it.rb
@@ -10,32 +10,45 @@
 # dashatize(6815) -> '68-1-5'
 
 def dashatize(num)
+  if num.nil?
+    return "nil"
+  end
   number_collection = []
-  num_array = num.digits.reverse
-  # num_array = num.to_s.chars
-  num_array.map do |number|
-    if number.odd?
-      number = "-" + "#{number}" + "-"
+  no_negative_numbers = num.to_s.delete("-").to_i
+  num_array = no_negative_numbers.digits.reverse
+  num_array.map.with_index do |number, index|
+    if number.odd? && number_collection.empty?
+      number = "#{number}" + "-"
       number_collection << number
+    elsif number.odd?
+      if number_collection.last[-1] == "-"
+        number = "#{number}" + "-"
+        number_collection << number
+      else
+        number = "-" + "#{number}" + "-"
+        number_collection << number
+      end
     else
       number_collection << number.to_s
     end
   end
-  number_collection.flatten.join
-  # num.gsub(/["13579"](.)/) {$1.push("0000")}
-  # num.gsub(/[02468](.)/) {"#{$1.gsub($1, "-#{$1}")}"}
+  number_collection = number_collection.flatten.join.strip
+  if number_collection[-1] == "-"
+    number_collection[-1] = ""
+  end
+  number_collection
 end
 
-#basic
+# basic
 
 p dashatize(274)# "2-7-4"
 p dashatize(5311)# ,"5-3-1-1"
-# p dashatize(86320)# "86-3-20"
-# p dashatize(974302)# "9-7-4-3-02"
+p dashatize(86320)# "86-3-20"
+p dashatize(974302)# "9-7-4-3-02"
 
 #weird
 
-# p dashatize(nil)# "nil"
-# p dashatize(0)# "0"
-# p dashatize(-1)# "1"
-# p dashatize(-28369)# "28-3-6-9"
+p dashatize(nil)# "nil"
+p dashatize(0)# "0"
+p dashatize(-1)# "1"
+p dashatize(-28369)# "28-3-6-9"
