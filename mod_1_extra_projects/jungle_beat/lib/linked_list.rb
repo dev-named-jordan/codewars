c00d5ef740801cd9177aef60b31a940523b808eb
Jordan Beck <jordanfbeck@gmail.com>
2021-12-22 10:39:59 -0700

    Adds working and tested methods for linked_list - count and to_string

--- mod_1_extra_projects/jungle_beat/lib/linked_list.rb
+++ mod_1_extra_projects/jungle_beat/lib/linked_list.rb
@@ -5,13 +5,44 @@ class LinkedList
         self.head = nil
     end 
     
-    def to_string(linked_list)
-        # require "pry"; binding.pry
-        linked_list.head.data
-    end 
+    def to_string(string_collection = [])
+        unless self.head.nil?
+            tail_node = self.head
+            og_head = self.head
+            string_collection.push(tail_node.data)
+            until tail_node.next_node.nil?
+                tail_node = tail_node.next_node
+                string_collection.push(tail_node.data)
+                string_collection
+            end
+            self.head = og_head
+            string_collection.join(" ")
+        end
+    end
+
+    # def to_string(linked_list, string_list = [])
+    #     if linked_list.head && linked_list.head.next_node.nil? && string_list == []
+    #         # require "pry"; binding.pry
+    #         linked_list.head.data
+    #     elsif linked_list.head && linked_list.head.next_node.nil? && string_list[0]
+    #         string_list.push(linked_list.data)
+    #         string_list.join(" ")
+    #     elsif !linked_list.head.next_node.nil?
+    #         string_list.push(linked_list.head.data)
+    #         linked_list = linked_list.head.next_node
+    #         if linked_list.next_node.nil?
+    #             # require "pry"; binding.pry
+    #             string_list.push(linked_list.data)
+    #             string_list.join(" ")
+    #         else
+    #             # require "pry"; binding.pry
+    #             linked_list.to_string(linked_list, string_list)
+    #         end
+    #     end 
+    #     require "pry"; binding.pry
+    # end 
 
     def append(data)
-        # require "pry"; binding.pry
         if head.nil?
             self.head = Node.new(data, nil)
         else 
@@ -23,29 +54,70 @@ class LinkedList
         end
     end
 
-    def count(counter = 0, og_head = 0)
-        if head.nil?
-            return counter
-        elsif head.next_node.nil?
-            counter += 1
-             if og_head.class != Integer
-                # require "pry"; binding.pry
-                self.head = og_head 
-            end
-            return counter
-        else
-        counter += 1
-            if !self.head.next_node.nil?   
-                og_head = self.head
-                self.head = self.head.next_node
-                self.count(counter, og_head)
-            else
+    def count(counter = 0)
+        unless self.head.nil?
+            tail_node = self.head
+            og_head = self.head 
+            until tail_node.next_node.nil?
                 counter += 1
-                self.head = og_head
-                return counter
-            end 
-        end 
+                tail_node = tail_node.next_node
+            end
+            counter += 1
+            counter
+        else
+            counter
+        end
     end
+
+    # def count(counter = 0, og_head = 0, new_head = 0)
+    #     # require "pry"; binding.pry
+    #     if og_head != 0 && head.next_node.nil?
+    #         require "pry"; binding.pry
+    #         counter += 1
+    #         self.head = og_head
+    #         counter
+    #     elsif og_head != 0 && head.next_node.next_node.nil?
+    #         require "pry"; binding.pry
+    #         counter += 1
+    #         self.head = og_head
+    #         counter
+    #     elsif og_head != 0 && head.next_node.next_node.next_node.nil?
+    #         require "pry"; binding.pry
+    #         counter += 2
+    #         self.head = og_head
+    #         counter
+    #     elsif head.nil?
+    #         require "pry"; binding.pry
+    #         counter
+    #     elsif head.next_node.nil?
+    #         require "pry"; binding.pry
+    #         counter += 1
+    #         counter 
+    #     else
+    #     # counter += 1
+    #         if !self.head.next_node.nil? 
+    #             counter += 1  
+    #             # require "pry"; binding.pry
+    #             if self.head.next_node.next_node.nil?
+    #                 # require "pry"; binding.pry
+    #                 # self.head = og_head
+    #                 counter += 1
+    #                 counter
+    #             else
+    #                 counter += 1
+    #                 # require "pry"; binding.pry
+    #                 og_head = self
+    #                 new_head = self.head
+    #                 self.count(counter, og_head, new_head)
+    #             end
+    #         # else
+    #         #     require "pry"; binding.pry
+    #         #     counter += 1
+    #         #     self.head = og_head
+    #         #     counter
+    #         end 
+    #     end 
+    # end
 
     private 
 
@@ -64,7 +136,4 @@ end
 # ll.append(20)
 # ll.append(30)
 #  puts ll.count
-# puts ll.head.data
-# puts ll.head.next_node.data
-# puts ll.head.next_node.next_node.data
-# puts ll.head.next_node.next_node.next_node.nil?
+#  puts ll.to_string
--- mod_1_extra_projects/jungle_beat/test/linked_list_test.rb
+++ mod_1_extra_projects/jungle_beat/test/linked_list_test.rb
@@ -4,39 +4,62 @@ require './lib/linked_list'
 require './lib/node'
 
 class LinkedListTest < Minitest::Test
-
+  
   def setup
     @linked_list = LinkedList.new
-  end
-
-  def test_it_exists_and_has_attributes
+    assert_equal 0, @linked_list.count
     assert_instance_of LinkedList, @linked_list
     assert_nil @linked_list.head
-    assert_equal 0, @linked_list.count
-    
+    assert_nil @linked_list.to_string
     @node_1 = @linked_list.append("plop")
-
-    assert_equal 1, @linked_list.count
+    assert_equal "plop", @linked_list.to_string
+    assert_instance_of LinkedList::Node, @linked_list.head
     assert_instance_of LinkedList::Node, @node_1
     assert_equal "plop", @node_1.data
+    assert_equal 1, @linked_list.count
     assert_nil @node_1.next_node
-    # require "pry"; binding.pry
-    assert_equal "plop", @linked_list.to_string(@linked_list)
     @node_2 = @linked_list.append("ping")
-    
+    assert_equal "plop ping", @linked_list.to_string
+    assert_equal "ping", @linked_list.head.next_node.data
     assert_equal 2, @linked_list.count
+    assert_nil @node_2.next_node
+    assert_nil @linked_list.head.next_node.next_node
+    @node_3 = @linked_list.append("doop")
+    assert_equal "plop ping doop", @linked_list.to_string
+    assert_equal 3, @linked_list.count
+    assert_equal "plop", @linked_list.head.data
+  end
+  
+  def test_it_can_append_nodes
+    assert_instance_of LinkedList::Node, @linked_list.head
+    assert_equal LinkedList::Node, @linked_list.head.next_node.class
+    assert_instance_of LinkedList::Node, @node_1
+    assert_instance_of LinkedList::Node, @node_2
     assert_instance_of LinkedList::Node, @node_1.next_node
     assert_equal "plop", @node_1.data
     assert_equal "ping", @node_2.data
-    assert_instance_of LinkedList::Node, @linked_list.head
+    assert_equal "doop", @node_3.data
     assert_equal "plop", @linked_list.head.data
-    assert_equal LinkedList::Node, @linked_list.head.next_node.class
     assert_equal "ping", @linked_list.head.next_node.data
-    assert_nil @linked_list.head.next_node.next_node
-
-    @node_3 = @linked_list.append("doop")
-
+    assert_equal @node_2, @node_1.next_node
     assert_equal 3, @linked_list.count
-    # assert_eqaul "doop", @linked_list.to_string
+    assert_instance_of LinkedList::Node, @linked_list.head
+    assert_equal LinkedList::Node, @linked_list.head.next_node.class
+    assert_equal LinkedList::Node, @linked_list.head.next_node.next_node.class
+    assert_instance_of LinkedList::Node, @node_1
+    assert_instance_of LinkedList::Node, @node_2
+    assert_instance_of LinkedList::Node, @node_3
+    assert_instance_of LinkedList::Node, @node_1.next_node
+    assert_instance_of LinkedList::Node, @node_1.next_node.next_node
+    assert_equal "plop", @node_1.data
+    assert_equal "ping", @node_2.data
+    assert_equal "doop", @node_3.data
+    assert_equal "plop", @linked_list.head.data
+    assert_equal "ping", @linked_list.head.next_node.data
+    assert_equal "doop", @linked_list.head.next_node.next_node.data
+    assert_equal @node_2, @node_1.next_node
+    assert_equal @node_3, @node_1.next_node.next_node
+    assert_nil @node_2.next_node.next_node
+    assert_nil @linked_list.head.next_node.next_node.next_node
   end
 end
