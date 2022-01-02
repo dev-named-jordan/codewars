be6ec18dc1e64b57c4d8e5f40cdc92ab445478e2
Jordan Beck <jordanfbeck@gmail.com>
2022-01-02 11:02:28 -0700

    Adds wrapper to jungle beat with append and count methods

---
+++ mod_1_extra_projects/jungle_beat/lib/jungle_beat.rb
@@ -0,0 +1,20 @@
+class JungleBeat
+
+  attr_reader :list
+
+  def initialize
+    @list = LinkedList.new
+  end 
+
+  def append(string)
+    string_array = string.split(" ")
+    string_array.select do |word|
+      @list.append(word)
+    end 
+    string
+  end
+
+  def count
+    @list.count
+  end
+end 
--- mod_1_extra_projects/jungle_beat/test/linked_list_test.rb
+++ mod_1_extra_projects/jungle_beat/test/linked_list_test.rb
@@ -2,6 +2,8 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/linked_list'
 require './lib/node'
+require "./lib/jungle_beat"
+
 
 class LinkedListTest < Minitest::Test
   
@@ -94,5 +96,16 @@ class LinkedListTest < Minitest::Test
     assert_equal "dom woo dop plop poo ping", @linked_list.to_string  
     assert_equal "ping", @linked_list.pop
     assert_equal "dom woo dop plop poo", @linked_list.to_string  
+  end
+
+  def test_jungle_beat_wrapper
+    jb = JungleBeat.new
+    assert_equal LinkedList, jb.list.class
+    assert_nil jb.list.head
+    assert_equal "deep doo ditt", jb.append("deep doo ditt")    
+    assert_equal "deep", jb.list.head.data    
+    assert_equal "doo", jb.list.head.next_node.data    
+    assert_equal "woo hoo shu", jb.append("woo hoo shu")    
+    assert_equal 6, jb.count   
   end
 end
